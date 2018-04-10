7f137bd6b45f8830f1f0a77aac54974c079d0a51
Márton Elek <elek@apache.org>
2018-04-10 23:46:37 +0200

    handle versions in the update script

--- data/component/update.sh
+++ data/component/update.sh
@@ -7,4 +7,8 @@ for COMPONENT in $(ls -1 | grep -v update); do
 	sed -i  "s/\(launcherversion:\).*/\1 $LAUNCHER_VERSION/g" $NAME.yaml
 	BASE_VERSION=`docker inspect --format "{{ index .Config.Labels \"io.github.flokkr.base.version\"}}" flokkr/$NAME`
 	sed -i  "s/\(baseversion:\).*/\1 $BASE_VERSION/g" $NAME.yaml
+	VERSION=`docker inspect --format "{{ index .Config.Labels \"io.github.flokkr.$NAME.version\"}}" flokkr/$NAME`
+	if [ "$VERSION" ]; then
+	   sed -i  "s/\(latest:\).*/\1 $VERSION/g" $NAME.yaml	
+   fi
 done
